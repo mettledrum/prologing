cd98543575097e7cd68f3ca77f43298572782204
mettledrum <mettledrum@gmail.com>
2018-11-04 12:03:17 -0700

    more Ch6 answers

--- learn_prolog_now!/6_more_lists.pl
+++ learn_prolog_now!/6_more_lists.pl
@@ -13,11 +13,26 @@ accRev([H|T],A,R) :- accRev(T,[H|A],R).
 accRev([],L,L).
 rev(L,R) :- accRev(L,[],R).
 
+% suffix
+suff(S,L) :- app(_, S, L).
+
+% prefix
+pref(P,L) :- app(P, _, L).
+
+% subList
+subl(SubL,L) :- suff(S,L), pref(SubL,S).
+
 % 6.1 double the list
 dbl(L,D) :- app(L,L,D).
 
 % 6.2 palindrome detection
 pal(L) :- rev(L,L).
+
+% 6.3 shave off first and last elements of a list
+notFirst([_|T],T).
+notLast([H|T],[H|L]) :- notLast(T,L).
+notLast([_],[]).
+toptail(InL,OutL) :- notFirst(InL,X), notLast(X,OutL).
 
 % 6.4.1 last element in list using rev/2
 lastRev(L,H) :- rev(L,[H|_]).

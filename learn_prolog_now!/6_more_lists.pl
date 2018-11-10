e1ff80cd5051b7dd6a8b78862fe34ee3a2deac89
mettledrum <mettledrum@gmail.com>
2018-11-10 08:58:12 -0700

    add 6.5 and an incomplete 6.6

--- learn_prolog_now!/6_more_lists.pl
+++ learn_prolog_now!/6_more_lists.pl
@@ -1,9 +1,5 @@
 % Chapter 6: More Lists
 
-% wrong append; something weird with Tail
-% app([Hx|Tx],A,App) :- app(Tx,[A|Hx],App).
-% app([],L,L).
-
 % append
 app([H|T],L2,[H|L3]) :- app(T,L2,L3). % put same Heads on
 app([],L,L).                          % after copying the 2nd List
@@ -14,13 +10,15 @@ accRev([],L,L).
 rev(L,R) :- accRev(L,[],R).
 
 % suffix
-suff(S,L) :- app(_, S, L).
+suff(S,L) :- app(_,S,L).
 
 % prefix
-pref(P,L) :- app(P, _, L).
+pref(P,L) :- app(P,_,L).
 
 % subList
-subl(SubL,L) :- suff(S,L), pref(SubL,S).
+subList(SubL,L) :- suff(S,L), pref(SubL,S).
+
+% Exercises
 
 % 6.1 double the list
 dbl(L,D) :- app(L,L,D).
@@ -32,7 +30,7 @@ pal(L) :- rev(L,L).
 notFirst([_|T],T).
 notLast([H|T],[H|L]) :- notLast(T,L).
 notLast([_],[]).
-toptail(InL,OutL) :- notFirst(InL,X), notLast(X,OutL).
+topTail(InL,OutL) :- notFirst(InL,X), notLast(X,OutL).
 
 % 6.4.1 last element in list using rev/2
 lastRev(L,H) :- rev(L,[H|_]).
@@ -40,3 +38,43 @@ lastRev(L,H) :- rev(L,[H|_]).
 % 6.4.2 last element in list using recursion
 lastRec([X],X).
 lastRec([_|T],X) :- lastRec(T,X).
+
+% 6.5 see if lists are the same except for first and last elements being swapped
+firstOne([H|_],H).
+lastOne([X],X).
+lastOne([_|T],X) :- last(T,X).
+swapfl(InL,OutL) :- topTail(InL,X), topTail(OutL,X), 
+                    firstOne(InL,Y), lastOne(OutL,Y).
+
+% 6.6 who keeps the zebra?
+isMember([X|_], X).
+isMember([_|T],X) :- isMember(T,X).
+
+% see if X2 is right of X1 in the list
+isRight([X1|T],X1,X2) :- isMember(T,X2).
+isRight([_|T],X1,X2) :- isRight(T,X1,X2).
+
+color(red).
+color(blue).
+color(green).
+natl(english).
+natl(japanese).
+natl(spanish).
+pet(snail).
+pet(zebra).
+pet(jaguar).
+
+% The Englishman lives in the red house.
+% house(color(red),natl(english),pet(_)).
+
+% The jaguar is the pet of the Spanish family.
+% house(color(_),natl(spanish),pet(jaguar)).
+
+% The Japanese lives to the right of the snail keeper.
+
+% The snail keeper lives to the left of the blue house.
+
+hood([H1,H2,H3]) :- isMember([H1,H2,H3], house(color(red),natl(english),pet(_))),
+                    isMember([H1,H2,H3], house(color(_),natl(spanish),pet(jaguar))),
+                    isRight([H1,H2,H3],  house(color(_),natl(_),pet(snail)), house(color(_),natl(japanese),pet(_))),
+                    isRight([H1,H2,H3],  house(color(_),natl(_),pet(snail)), house(color(blue),natl(_),pet(_))).
